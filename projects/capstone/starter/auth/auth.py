930f28c81650261f8bc958c3abf132676a6665a8
leenah <loly.lele@gmail.com>
2021-09-02 04:33:47 +0300

    https://github.com/Ieenah/FSND

--- projects/capstone/starter/auth/auth.py
+++ projects/capstone/starter/auth/auth.py
@@ -1,4 +1,4 @@
-from os import environ
+import os
 import json
 from flask import request, abort
 from functools import wraps
@@ -6,9 +6,9 @@ from jose import jwt
 from urllib.request import urlopen
 
 
-AUTH0_DOMAIN = 'ileenah.us.auth0.com'
-ALGORITHMS = ['RS256']
-API_AUDIENCE = ('dev1')
+AUTH0_DOMAIN = os.environ['AUTH0_DOMAIN']
+ALGORITHMS = os.environ['ALGORITHMS']
+API_AUDIENCE = os.environ['API_AUDIENCE']
 
 ## AuthError Exception
 '''
